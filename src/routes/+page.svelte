d336047ebe9201b8874f5309db354efbfd57628f
kyoh86 <me@kyoh86.dev>
2024-12-02 01:36:49 +0900

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -76,105 +76,140 @@ const deleteHighlight = (id: string): void => {
 };
 </script>
 
-<style>
-    .preview-box {
-        width: 100%;
-        height: 100px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border: 1px solid #ccc;
-        margin-top: 1rem;
-    }
-    .palette-table {
-      display:grid;
-      grid-template-columns: repeat(2, minmax(0, auto) minmax(0, 1fr));
-      width: fit-content;
-      align-items: center;
-    }
-    .highlight-table {
-      display: grid;
-      align-items: center;
-      grid-template-columns: minmax(0, auto) minmax(0, auto);
-      width: fit-content;
-      row-gap: 1rem;
-    }
-    .highlight-item {
-        display: flex;
-        gap: 1rem;
-        align-items: center;
-        margin-bottom: 0.5rem;
-    }
-    .highlight-actions button {
-        margin-left: 0.5rem;
-    }
-</style>
-
 <div>
-    <h1>Customizable Palette Tester</h1>
+  <h1>Customizable Palette Tester</h1>
 
+  <h2 class="text-xl text-bold mt-4">Edit Palette</h2>
+  <div class="flex flex-col gap-4">
     <div class="palette-table">
-        {#each Object.keys(palette) as colorName (colorName)}
-                <label class="bortext-right" for="palette-{colorName}">{colorName}:</label>
-                <div class="mr-4">
-                  <ColorPicker
-                    label="Pick a color"
-                    name="palette-{colorName}"
-                    hex="{palette[colorName as PaletteKey]}"
-                    on:input="{({ detail }) => {
-                        if (detail.hex) {
-                            palette[colorName as PaletteKey] = detail.hex;
-                        }
-                    }}"
-                  />
-                </div>
-        {/each}
+      {#each Object.keys(palette) as colorName (colorName)}
+        <label class="text-right" for="palette-{colorName}">{colorName}:</label>
+        <div>
+          <ColorPicker
+            label="Pick a color"
+            name="palette-{colorName}"
+            hex={palette[colorName as PaletteKey]}
+            on:input={({ detail }) => {
+              if (detail.hex) {
+                palette[colorName as PaletteKey] = detail.hex;
+                updateContrastRatio();
+              }
+            }}
+          />
+        </div>
+        <div>
+          <label>
+            <input
+              type="radio"
+              name="foreground"
+              value={colorName}
+              bind:group={selectedForeground}
+              on:change={updateContrastRatio}
+            />
+            Foreground
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="background"
+              value={colorName}
+              bind:group={selectedBackground}
+              on:change={updateContrastRatio}
+            />
+            Background
+          </label>
+        </div>
+      {/each}
     </div>
 
-    <h2 class="text-xl mt-4">Create Highlight</h2>
+    <h2 class="text-xl text-bold mt-4">Edit Highlight</h2>
     <div class="highlight-table">
-        <label for="foreground">Foreground:</label>
-        <select id="foreground" bind:value={selectedForeground} on:change={updateContrastRatio}>
-            {#each Object.keys(palette) as colorName}
-                <option value={colorName as PaletteKey}>{colorName}</option>
-            {/each}
-        </select>
-        <label for="background">Background:</label>
-        <select id="background" bind:value={selectedBackground} on:change={updateContrastRatio}>
-            {#each Object.keys(palette) as colorName}
-                <option value={colorName as PaletteKey}>{colorName}</option>
-            {/each}
-        </select>
-    </div>
-
-    <div
+      <div>Preview:</div>
+      <div
         class="preview-box"
-        style="background-color: {palette[selectedBackground]}; color: {palette[selectedForeground]};"
-    >
+        style="background-color: {palette[selectedBackground]}; color: {palette[
+          selectedForeground
+        ]};"
+      >
         Sample Text
-    </div>
-    <p>Contrast Ratio: {contrastRatio}</p>
-
-    <div>
-        <input type="text" placeholder="Highlight name" bind:value={highlightName} />
+      </div>
+      <div>Contrast:</div>
+      <div style="display: flex; align-items: center; gap: 1rem;">
+          <div>{contrastRatio}</div>
+          <div
+              style="
+                  width: 100px;
+                  height: 10px;
+                  background-color: {parseFloat(contrastRatio) >= 4.5 ? 'green' : parseFloat(contrastRatio) >= 3 ? 'yellow' : 'red'};
+              "
+          ></div>
+      </div>
+      <label for="highlight-name">Name:</label>
+      <div>
+        <input
+          type="text"
+          name="highlight-name"
+          placeholder="Highlight name"
+          bind:value={highlightName}
+        />
         <button on:click={() => saveHighlight(highlightName)}>
-            {editingHighlight ? "Update Highlight" : "Save Highlight"}
+          {editingHighlight ? "Update Highlight" : "Save Highlight"}
         </button>
+      </div>
     </div>
 
-    <h2>Saved Highlights</h2>
+    <div></div>
+
+    <h2 class="text-xl text-bold mt-4">Saved Highlights</h2>
     <div>
-        {#each highlights as highlight (highlight.id)}
-            <div
-                class="highlight-item"
-                style="background-color: {palette[highlight.background]}; color: {palette[highlight.foreground]};"
+      {#each highlights as highlight (highlight.id)}
+        <div
+          class="highlight-item"
+          style="background-color: {palette[
+            highlight.background
+          ]}; color: {palette[highlight.foreground]};"
+        >
+          <span>{highlight.name}</span>
+          <div class="highlight-actions">
+            <button on:click={() => editHighlight(highlight)}>Edit</button>
+            <button on:click={() => deleteHighlight(highlight.id)}
+              >Delete</button
             >
-                <span>{highlight.name}</span>
-                <div class="highlight-actions">
-                    <button on:click={() => editHighlight(highlight)}>Edit</button>
-                    <button on:click={() => deleteHighlight(highlight.id)}>Delete</button>
-                </div>
-            </div>
-        {/each}
+          </div>
+        </div>
+      {/each}
     </div>
+  </div>
 </div>
+
+<style>
+  .palette-table {
+    display: grid;
+    grid-template-columns: minmax(0, auto) minmax(0, auto) 1fr;
+    column-gap: 10px;
+    row-gap: 5px;
+    align-items: center;
+    width: fit-content;
+  }
+  .preview-box {
+    padding: 1rem 2rem;
+    width: fit-content;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #ccc;
+  }
+  .highlight-table {
+    display: grid;
+    column-gap: 10px;
+    row-gap: 5px;
+    grid-template-columns: minmax(0, auto) minmax(0, auto);
+    width: fit-content;
+    align-items: center;
+  }
+  .highlight-item {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+  }
+</style>
